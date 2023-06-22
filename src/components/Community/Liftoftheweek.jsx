aa57a77f3419ac13abba0c36fb3894b2087befbb
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-06-22 11:11:00 +0530

    added video in lift of the week

--- src/components/Community/Liftoftheweek.jsx
+++ src/components/Community/Liftoftheweek.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 function Liftoftheweek() {
+  URL = "https://www.youtube.com/embed/rM8T8WcGtbI";
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-3xl font-extrabold py-10 text-center">
         Lift Of the Week
       </h2>
-      <img
-        src="https://images.unsplash.com/photo-1595078475328-1ab05d0a6a0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3ltJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
-        alt=""
-        className="h-96 rounded-lg"
-      />
+      <iframe
+        className="w-full aspect-[9/16]"
+        src="https://www.youtube.com/embed/rM8T8WcGtbI?autoplay=1&mute=1"
+      ></iframe>
+
       <div className="flex flex-col items-center py-10">
         <p className="text-2xl font-bold">Charlotte Parker</p>
         <p className="text-lg font-bold text-gray-400"> Power Lifter</p>
