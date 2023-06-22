a5e1cea47ace9e62116fd62f87cd53625dc2bd55
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-06-22 16:10:36 +0530

    fixed blogs page

--- src/components/Blogs/Hero.jsx
+++ src/components/Blogs/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         src="https://img.freepik.com/premium-photo/computer-tablet-is-putting-table-blurred-gym-as-background_35674-2792.jpg?w=1800"
         className="absolute h-[80vh] z-0 w-full object-cover"
       />
-      <div className="h-[80vh] z-20 gap-y-10 flex flex-col pt-20 items-center">
+      <div className="h-[80vh] z-20 gap-y-10 flex flex-col pt-12 items-center">
         <h1 className="font-extrabold text-center text-5xl p-5">
           Ignite Your Fitness Journey with Minumum Strength
         </h1>
@@ -18,10 +18,7 @@ function Hero() {
           success stories, and practical tips to help you achieve your fitness
           goals. Whether you're a beginner or a seasoned fitness enthusiast, our
           passionate team is here to guide and empower you every step of the
-          way. Let's unlock your full potential and embark on a remarkable
-          transformation together. Get ready to sweat, smile, and surpass your
-          limits as you embrace a healthier, stronger, and more vibrant version
-          of yourself.
+          way.
         </p>
       </div>
     </>
