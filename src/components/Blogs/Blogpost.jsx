09d0e80fec29b8c8e4e0cb4affee7eac157dbb89
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-07-11 20:35:10 +0530

    fixed blogpost page

--- src/components/Blogs/Blogpost.jsx
+++ src/components/Blogs/Blogpost.jsx
@@ -6,15 +6,15 @@ export default function BlogPost({ data }) {
   const post = data.allWpPost;
   return (
     <Layout>
-      <div className="bg-gray-800 text-white min-h-screen max-w-xl mx-auto w-full">
+      <div className="bg-gray-800 text-white min-h-screen">
         {post.edges.map((blog) => (
-          <div className="flex flex-col items-center justify-center px-5">
-            <h1 className="text-2xl font-extrabold pt-10 w-full flex justify-center">
+          <div className="flex flex-col items-center justify-center px-5 max-w-5xl mx-auto">
+            <h1 className="text-2xl lg:text-5xl font-extrabold pt-10 w-full flex justify-center">
               {blog.node.title}
             </h1>
             <h4 className="text-lg font-light text-gray-400">DD/MM/YYYY</h4>
             <div
-              className="text-center space-y-5 flex flex-col justify-center items-center mt-10 [&_img]:rounded-2xl [&:nth-child(3)]:text-justify"
+              className="text-center lg:text-xl space-y-5 flex flex-col justify-center items-center mt-10 [&_img]:rounded-2xl [&:nth-child(3)]:text-justify"
               dangerouslySetInnerHTML={{ __html: blog.node.content }}
             />
           </div>
