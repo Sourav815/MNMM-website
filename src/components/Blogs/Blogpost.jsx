d335e85dfb49f73b1a4c36dec03b47a8b51d419e
Sourav <souravbhunia162@gmail.com>
2023-07-13 01:41:00 +0530

    change style of blogpost page

--- src/components/Blogs/Blogpost.jsx
+++ src/components/Blogs/Blogpost.jsx
@@ -12,9 +12,9 @@ export default function BlogPost({ data }) {
             <h1 className="text-2xl lg:text-5xl font-extrabold pt-10 w-full flex justify-center">
               {blog.node.title}
             </h1>
-            <h4 className="text-lg font-light text-gray-400">DD/MM/YYYY</h4>
+            {/* <h4 className="text-lg font-light text-gray-400">DD/MM/YYYY</h4> */}
             <div
-              className="text-center lg:text-xl space-y-5 flex flex-col justify-center items-center mt-10 [&_img]:rounded-2xl [&:nth-child(3)]:text-justify"
+              className="text-center lg:text-xl space-y-5 flex flex-col justify-center items-center mt-10 [&_figure_img]:rounded-2xl [&_div_div_img]:rounded-full [&_p]:text-justify [&>div]:flex [&>div]:items-center [&>div]:ml-[-8rem] [&_strong]:text-xs [&_div_div]:ml-1 [&_ul]:flex [&_ul]:w-full [&_ul]:justify-around [&_li]:mb-8" 
               dangerouslySetInnerHTML={{ __html: blog.node.content }}
             />
           </div>
